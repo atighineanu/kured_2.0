7939f13ee7cabb3ca4c537d0847f0ed7a2d41b8d
atighineanu <alexei.tighineanu@gmail.com>
2022-06-08 11:49:52 +0200

    improved rebootrequired checker

--- tmp/main.go
+++ tmp/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"strings"
+	"text/template"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -19,17 +22,28 @@ scheduled="no"
 timeFrame=""`
 )
 
+const (
+	stateStrTemplate = `nodes={{.Nodes}}
+value={{.Value}}
+period={{.Period}}
+scheduled={{.Scheduled}}
+timeFrame={{.TimeFrame}}
+nodeInProcess={{.NodeInProcess}}`
+)
+
 type State struct {
-	Nodes     []string
-	Value     string
-	Period    string
-	Scheduled string
-	TimeFrame string
+	Nodes         []string
+	Value         string
+	Period        string
+	Scheduled     string
+	TimeFrame     string
+	NodeInProcess string
 }
 
-func convert(mapData string) State {
+// parsing configMap values
+func parseConfigMapVals(mapData string) State {
 	state := &State{}
-	for _, row := range strings.Split(input, "\n") {
+	for _, row := range strings.Split(mapData, "\n") {
 		splittedRow := strings.Split(row, "=")
 		if len(splittedRow) != 2 {
 			continue // if the input is not properly formatted - do nothing
@@ -37,12 +51,6 @@ func convert(mapData string) State {
 			switch splittedRow[0] {
 			case "nodes":
 				state.Nodes = append(state.Nodes, strings.Split(splittedRow[1], ",")...)
-				//for _, node := range state.Nodes {
-				//fmt.Println(node)
-				//if nodeID == node {
-				//	fmt.Println("AHOY!!!")
-				//}
-				//}
 			case "value":
 				state.Value = splittedRow[1]
 			case "period":
@@ -51,6 +59,8 @@ func convert(mapData string) State {
 				state.Scheduled = splittedRow[1]
 			case "timeFrame":
 				state.TimeFrame = splittedRow[1]
+			case "nodeInProcess":
+				state.NodeInProcess = splittedRow[1]
 			}
 		}
 	}
@@ -58,27 +68,126 @@ func convert(mapData string) State {
 	return *state
 }
 
-func main() {
-	config, err := clientcmd.BuildConfigFromFlags("", "/home/user/.kube/config")
+func packConfigMapVals(state State) (stringState string) {
+	var tpl bytes.Buffer
+	tmpl, err := template.New("stateTempl").Parse(stateStrTemplate)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+	err = tmpl.Execute(&tpl, state)
+	if err != nil {
+		panic(err)
+	}
+	stringState = tpl.String()
+	return
+}
+
+func stripBracketsFromString(state string) (processedState string) {
+	for index, row := range strings.Split(state, "\n") {
+		if strings.Contains(row, "nodes=[") {
+			replacer := strings.NewReplacer("[", "", "]", "", " ", ",")
+			row = replacer.Replace(row)
+		}
+		if index < (len(strings.Split(state, "\n")) - 1) {
+			processedState += row + "\n"
+		} else {
+			processedState += row
+		}
+	}
+	return
+}
+
+func setConfigMapKey(kubeconfPath, configMapName, namespace, stateKey, stringState string) (err error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfPath)
+	if err != nil {
+		return
 	}
 	client, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 
 	ctx := context.Background()
-	configMaps, err := client.CoreV1().ConfigMaps("kube-system").List(ctx, metav1.ListOptions{})
+	configMaps, err := client.CoreV1().ConfigMaps(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+	for _, configMap := range configMaps.Items {
+		if configMap.GetName() == configMapName {
+			configMap.Data[stateKey] = stringState
+			_, err = client.CoreV1().ConfigMaps(namespace).Update(ctx, &configMap, metav1.UpdateOptions{})
+			if err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func returnConfigMapKey(kubeconfPath, key, configMapName, namespace string) (state State, err error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfPath)
+	if err != nil {
+		return
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return
+	}
+
+	ctx := context.Background()
+	configMaps, err := client.CoreV1().ConfigMaps(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	for _, configMap := range configMaps.Items {
+		if configMap.GetName() == configMapName {
+			state = parseConfigMapVals(configMap.Data[key])
+		}
+	}
+	return
+}
+
+func rebootRequired(nodeID, kubeconfPath, configMapName, namespace, stateKey, stateVal string) (error, bool) {
+	for i := 1; i < 5; i++ {
+		if os.Getenv(fmt.Sprintf("STATE%v", i)) != "" {
+			stateA := parseConfigMapVals(os.Getenv(fmt.Sprintf("STATE%v", i)))
+			log.Printf("HERE: %+v\n", stateA)
+			if stateA.Value == "reboot" && stateA.NodeInProcess == "" {
+				stateA.NodeInProcess = nodeID
+				err := setConfigMapKey(kubeconfPath, configMapName, namespace, stateKey, stripBracketsFromString(packConfigMapVals(stateA)))
+				if err != nil {
+					return nil, false
+				}
+				return nil, true
+			} else {
+				if stateA.NodeInProcess == nodeID {
+					return nil, true
+				}
+			}
+		}
+
+	}
+	return nil, false
+}
+
+func main() {
+	state, err := returnConfigMapKey("/home/user/.kube/config", "state2", "kured-brain", "kube-system")
 	if err != nil {
 		log.Printf("Error: %v\n", err)
 	}
-
-	var state State
-	for _, configMap := range configMaps.Items {
-		if configMap.Name == "kured-brain" {
-			state = convert(configMap.Data["state2"])
-		}
-	}
 	fmt.Printf("%+v\n", state)
+	state.Nodes = []string{"test-masters-0", "test-workers-0", "test-workers-1"}
+	strState := packConfigMapVals(state)
+	fmt.Printf("STRSTATE: %+v\n", strState)
+	strState = stripBracketsFromString(strState)
+	fmt.Printf("STRSTATEPROCESSED: %+v\n", strState)
+	err = setConfigMapKey("/home/user/.kube/config", "kured-brain", "kube-system", "state2", strState)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	err, _ = rebootRequired(state)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
 }
